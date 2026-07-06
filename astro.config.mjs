38b86f79692627636258040ff86f5ce03f4e0522
Izzysaac <icolmenaresga@gmail.com>
2026-07-06 03:47:02 -0500

    fix seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://alejandroscoffee.vercel.app",
+	site: "https://parrillasydefensas.com",
 	vite: {
 		plugins: [tailwindcss()],
 	},
